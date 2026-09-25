95030b5741a62356073c2d052b5affac23e434bb
Nishant <bhatinishant38@gmail.com>
2026-09-25 23:42:37 +0530

    nested routes

---
+++ 2-routes/app/services/page.js
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+import React from 'react'
+
+const services = () => {
+  return (
+    <>
+      <div>services</div>
+      <Link href="/">Home</Link>
+ 
+    </>
+   )
+}
+
+export default services
